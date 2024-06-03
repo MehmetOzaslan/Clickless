8becea58d9b50e81b86bd1eb4b8a55976c8518b8
Mehmet Ozaslan <mozaslan@mines.edu>
2024-06-02 19:02:22 -0600

    Add project files.

---
+++ src/MouseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace Clickless
+{
+    public static class MouseController
+    {
+        [DllImport("user32.dll")]
+        public static extern int SetCursorPos(int x, int y);
+
+        //Move the cursor to the given coordinates.
+        public static void MoveCursor(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+    }
+}
---
+++ src/main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clickless.src
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine(args.Length);
+            MouseController.MoveCursor(1, 1);
+        }
+    }
+
+}
