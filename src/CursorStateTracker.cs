8300b7c79b1c37dc97af9c58135afbcca780e0f1
Mehmet Ozaslan <mozaslan@mines.edu>
2024-07-08 14:35:27 -0600

    Added concurrent dictionary to the tracking of the cursor to allow for multithreaded iteration, this doesn't work as I'd like it to since the cursor probably exists on one thread in the OS somewhere..

--- src/CursorStateTracker.cs
+++ src/CursorStateTracker.cs
@@ -13,6 +13,7 @@ using System.Security.Cryptography;
 using static Clickless.MouseController;
 using System.IO;
 using static Util.MathUtil;
+using System.Collections.Concurrent;
 
 namespace Clickless.src
 {
@@ -22,20 +23,25 @@ namespace Clickless.src
     /// </summary>
     public class CursorStateTracker : ICursorUpdator
     {
-        private Dictionary<Vector2, IntPtr> _cursorPositionStates;
+        //private Dictionary<Vector2, IntPtr> _cursorPositionStates;
+        private ConcurrentDictionary<Vector2, IntPtr> _cursorPositionStates;
+        private int _initialCapacity = 2000;
+        private int concurrencyLevel = 5;
+
 
         public CursorStateTracker() {
-            _cursorPositionStates = new Dictionary<Vector2, IntPtr>();
+            _cursorPositionStates = new ConcurrentDictionary<Vector2, IntPtr>(concurrencyLevel, _initialCapacity);
         }
+
 
         public Dictionary<Vector2, IntPtr> GetPositionStates()
         {
-            return _cursorPositionStates; 
+            return _cursorPositionStates.ToDictionary(kvp => kvp.Key, kvp => kvp.Value); 
         }
 
         public void Update(Vector2 pos, CURSORINFO info)
         {
-            _cursorPositionStates.Add(pos, info.hCursor);
+            _cursorPositionStates.TryAdd(pos, info.hCursor);
         }
     }
 }
