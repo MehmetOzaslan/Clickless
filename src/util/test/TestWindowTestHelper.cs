21c519fc3224fbdcb6dbb46bee744d231ae5d647
Mehmet Ozaslan <mozaslan@mines.edu>
2024-07-08 14:30:16 -0600

    Added windows form component to assist with testing. Added tests for the testing component.

---
+++ src/util/test/TestWindowTestHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NUnit.Tests
+{
+    using System;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using Clickless;
+    using Clickless.src;
+    using Clickless.src.util.test;
+    using NUnit.Framework;
+    using static Util.MathUtil;
+
+    [TestFixture]
+    public class TestWindowTestHelper
+    {
+        [Test]
+        public void TestOne()
+        {
+            WindowTestHelper.ShowMessage("Test One");
+        }
+
+        [Test]
+        public void TestMany()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                WindowTestHelper.ShowMessage("Test " + i );
+            }
+            WindowTestHelper.CloseAll();
+        }
+
+
+    }
+
+}
---
+++ src/util/test/WindowTestHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clickless.src.util.test
+{
+    /// <summary>
+    /// Helper class used to test window related operations 
+    /// (EG: Clicking buttons, obtaining window information, rect sizes, etc.)
+    /// </summary>
+    class WindowTestHelper : Form
+    {
+        public const int width = 300;
+        public const int height = 200;
+        private static Stack<WindowTestHelper> _windowTestHelpers = new Stack<WindowTestHelper>();
+        public const string _text = "Test Text";
+        private Label messageLabel;
+
+        public WindowTestHelper()
+        {
+            this.messageLabel = new Label();
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(20, 20);
+            this.Controls.Add(messageLabel);
+
+            this.Text = _text;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Size = new Size(width, height);
+        }
+
+        public static void ShowMessage(string message)
+        {
+            WindowTestHelper window = new WindowTestHelper();
+            window.messageLabel.Text = message;
+            window.Show();
+            window.BringToFront();
+        }
+
+        public static void ShowMessage(string message, string caption)
+        {
+            WindowTestHelper window = new WindowTestHelper();
+            window.messageLabel.Text = message;
+            window.Text = caption;
+            window.Show();
+            window.BringToFront();
+        }
+
+        public static void ShowMessage(string message, string caption, Point point)
+        {
+            WindowTestHelper window = new WindowTestHelper();
+            window.messageLabel.Text = message;
+            window.Text = caption;
+            window.StartPosition = FormStartPosition.Manual;
+            window.Location = point;
+            window.Show();
+            window.BringToFront();
+        }
+
+
+        public static void CloseMostRecent()
+        {
+            if (_windowTestHelpers == null) return;
+            if (_windowTestHelpers.Count <= 0) return;
+
+            var mostRecent = _windowTestHelpers.Pop();    
+            mostRecent?.Close();
+        }
+
+        public static void CloseAll()
+        {
+            if (_windowTestHelpers == null) return;
+            if (_windowTestHelpers.Count <= 0) return;
+
+            while (_windowTestHelpers.Count > 0)
+            {
+                var mostRecent = _windowTestHelpers.Pop();
+                mostRecent?.Close();
+            }   
+        }
+    }
+}
