4235d11f3422bc389adb0d9c6797e62cb1a9089b
Mehmet Ozaslan <mozaslan@mines.edu>
2024-07-15 19:48:43 -0600

    Added in some ways of rendering boxes to the screen with a transparent window overlay. Created rudimentary method to create command sequences. EG: "A" "AA" "AB".. etc, and some tests, this is still broken.

---
+++ src/TextRectGenerator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clickless.src
+{
+    /// <summary>
+    /// Given n objects, creates a distinct string starting with A, B, C, D... Z then AA, AB...
+    /// </summary>
+    /// 
+    public static class CommandGenerator
+    {
+        public const int ASCII_A = 65;
+
+        /// <summary>
+        /// Gets the logarithm of a number in base 26. Divides until the number is > 26.
+        /// </summary>
+        private static int Log26Rounded(int num)
+        {
+            int count = 0;
+            while (num > 26)
+            {
+                num/= 26;
+                count++;
+            }
+            return count;
+        }
+
+
+        public static List<string> GenerateCommands(int num) {
+            List<string> commands = new List<string>();
+            
+            int[] ints = new int[num];
+
+            int _currNum = 0;
+            for (int i = 0; i < num; i++) {
+                ints[i] = i;
+            }
+
+            //We are basically counting with base 26.
+            //26
+            //676
+            //17576
+
+            foreach (int i in ints) {
+
+                //Number of digits obtained through logarithm.
+                int digitCount = Log26Rounded(i);
+                int str_length = 0;
+
+                var command = "";
+                do
+                {
+                    if(digitCount == 0)
+                    {
+                        command += (char)((i) + ASCII_A);
+                    }
+                    else
+                    {
+                        command += (char)(i/(26*digitCount) + ASCII_A);
+                    }
+                    str_length += 1;
+                } while (str_length < digitCount);
+                commands.Add(command);
+            }
+
+
+            return commands;
+        }
+    }
+
+    internal class TextRectGenerator
+    {
+
+
+
+    }
+}
---
+++ src/util/test/TestCommandGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace NUnit.Tests
+{
+    using System;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Clickless;
+    using Clickless.src;
+    using NUnit.Framework;
+    using static Util.MathUtil;
+
+    [TestFixture]
+    internal class TestCommandGenerator
+    {
+        [Test]
+        public void TestGeneration()
+        {
+            int count = 1000;
+            var cmds = CommandGenerator.GenerateCommands(count);
+            Assert.AreEqual(count, cmds.Count);
+        }
+
+        [Test]
+        public void TestUniqueness()
+        {
+            HashSet<string> strings = new HashSet<string>();
+            int count = 1000;
+            var cmds = CommandGenerator.GenerateCommands(count);
+            var unique = cmds.Select(x => x).Distinct();
+
+            Assert.AreEqual(count, cmds.Count);
+            Assert.AreEqual(count, unique.Count());
+        }
+
+        [Test]
+        public void TestOrder()
+        {
+            int count = 20000;
+            var cmds = CommandGenerator.GenerateCommands(count);
+            Assert.AreEqual("A",cmds[0]);
+            Assert.AreEqual("AA", cmds[26]);
+            Assert.AreEqual("AAA", cmds[676]);
+            Assert.AreEqual("AAAA", cmds[17576]);
+        }
+
+
+    }
+}
